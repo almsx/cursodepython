3485d5355451a37c27521e6095ab523a5cea92bc
KMMX <kmmx@Mac-mini-de-KMMX.local>
2018-01-27 10:07:02 -0600

    Correcion de vowelsconsonants, ya valida espacios

--- RicardoExamen/VowelsConsonants.py
+++ RicardoExamen/VowelsConsonants.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 palabra = raw_input("Ingresa una palabra ")
-while not all(x.isalpha() for x in palabra):
+while not all(x.isalpha() or x.isspace() for x in palabra):
 	palabra = raw_input("Ingresa una palabra ")
 print "Las vocales son:"
 for caracter in palabra:
@@ -9,5 +9,5 @@ for caracter in palabra:
 
 print "Las consonantes son:"
 for caracter in palabra:
-        if caracter not in "aeiouAEIOU":
+        if caracter not in "aeiouAEIOU" and caracter != " ":
            print caracter + caracter.upper()
