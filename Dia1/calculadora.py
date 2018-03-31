c3844212f1e30f63be56c20b046b20f592c035c1
Alberto Luebbert M <albertoluebbert@ideashappy.com>
2018-03-31 10:15:04 -0600

    Dia 1

---
+++ Dia1/calculadora.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+#AUTHOR: José Antonio Castelo Vega
+
+a = 4
+b = 27
+
+suma = (a + b)
+resta = (a - b)
+multp = (a * b)
+div = float(a) / float(b)
+
+# Investigar esta operacion
+#porc = (a % b)
+
+#Estamos utiizanco str() para poder mostrar el resultado en pantalla ya que estamos concatenando cadenas
+print 'La suma de a = ' + str(a) + ' y b = ' + str(b) + ' es: ' + str(suma)
+print 'La resta de a = ' + str(a) + ' y b = ' + str(b) + ' es: ' + str(resta)
+print 'La multiplicacion de a = ' + str(a) + ' y b = ' + str(b) + ' es: ' + str(multp)
+print 'La division de a = ' + str(a) + ' y b = ' + str(b) + ' es: ' + str(div)
+#print 'El modulo de a = ' + str(a) + ' y b = ' + str(b) + ' es: ' + str(porc)
